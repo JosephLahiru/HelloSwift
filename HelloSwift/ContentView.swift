b5f16abfd1a947b862093510dae13ebc58dfca8e
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-02-03 00:16:13 +0530

    added colors

--- HelloSwift/ContentView.swift
+++ HelloSwift/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "person")
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world...!")
+                .foregroundColor(Color.green)
+                .padding(2)
+            Text("Hello, World...!").foregroundColor(Color.blue)
         }
         .padding()
     }
