082d8803aaaf3cfb7b7e6e888f05db1008de8738
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-02-02 23:50:38 +0530

    Initial Commit

---
+++ HelloSwift/HelloSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftApp.swift
+//  HelloSwift
+//
+//  Created by Joseph Rasanjana on 2023-02-02.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
